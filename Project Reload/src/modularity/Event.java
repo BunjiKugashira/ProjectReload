e82f049201443bfb85b7a988cc5d6d90f4d0c4aa
Alexander Otto <alex_otto@cityweb.de>
2015-01-22 13:20:16 +0100

    Initial Commit

---
+++ Project Reload/src/modularity/Event.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package modularity;
+
+import java.util.Hashtable;
+
+/**
+ * @author Alexander Otto
+ *
+ */
+public abstract class Event {
+	private static Hashtable<String, Reaction> reactions = new Hashtable<String, Reaction>();
+	
+	/*
+	 * TODO comment
+	 */
+	public Event(Object... pArgs) {
+		for(Reaction r : reactions.values()) {
+			r.react(pArgs);
+		}
+	}
+	
+	/*
+	 * TODO comment
+	 */
+	public static final synchronized boolean registerReaction(String pKey, Reaction pReact) {
+		assert(pKey != null);
+		assert(pReact != null);
+		
+		if (reactions.containsKey(pKey)) {
+			return false;
+		}
+		else {
+			reactions.put(pKey, pReact);
+			return true;
+		}
+	}
+	
+	/*
+	 * TODO comment
+	 */
+	public static final synchronized void removeReaction(String pKey) {
+		assert(pKey != null);
+		
+		reactions.remove(pKey);
+	}
+	
+	/*
+	 * TODO comment
+	 */
+	public abstract class Reaction {
+		public abstract void react(Object... args);
+	}
+}
