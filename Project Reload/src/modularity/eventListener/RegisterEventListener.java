5eb6fec8b94f2e0971c2df06bd6eac615c7553ed
Alexander Otto <alex_otto@cityweb.de>
2015-01-29 07:06:30 +0100

    trying out to load classes from a folder

--- Project Reload/src/modularity/eventListener/RegisterEventListener.java
+++ Project Reload/src/modularity/eventListener/RegisterEventListener.java
@@ -5,6 +5,7 @@ package modularity.eventListener;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -29,6 +30,50 @@ public class RegisterEventListener {
 	 */
 	public static void run() {
 		// TODO register Events
+		File classDir = new File("/users/myproject/classes/");
+		assert(classDir != null);
+		
+		try {
+			URL[] url = { classDir.toURI().toURL() };
+			URLClassLoader urlLoader = new URLClassLoader(url);
+
+			String filename;
+			for (File file : classDir.listFiles()) {
+			    filename = file.getName();
+			    if (!filename.endsWith(".class"))
+			        continue;
+
+			try {
+				Object instance = urlLoader
+				    .loadClass("org.mypackage." + filename)
+				    .getConstructor()
+				    .newInstance();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			}
+		} catch(MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
